5ffea4beef57232efbeeecd39deb848aef957f88
LuizhBrandao <luizh.brandaoo@gmail.com>
2024-10-01 11:29:23 -0300

    Tipos e Variaveis

--- trilha-java-basico/java-aprendendo-sintaxe/src/edu/luiz/primeirasemana/TiposVariaveis.java
+++ trilha-java-basico/java-aprendendo-sintaxe/src/edu/luiz/primeirasemana/TiposVariaveis.java
@@ -1,3 +1,26 @@
+package edu.luiz.primeirasemana;
+
 public class TiposVariaveis {
-    
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //classe String que representa texto na aplicação
+
+        String meuNome = "Luiz Brandão";
+        
+        double salarioMinimo = 2500.33;
+
+        // TiposEVariaveis.java
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+        
+        final double VALOR_DE_PI = 3.14;
+
+    }    
 }
