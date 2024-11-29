2679236cd926d0e8893418691786234273b52d24
LuizhBrandao <luizh.brandaoo@gmail.com>
2024-11-29 12:16:05 -0300

    Ajuste de variáveis e lógica

--- ControlandoFluxoDaAplicacao/src/Main.java
+++ ControlandoFluxoDaAplicacao/src/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
          Scanner leitor = new Scanner(System.in);
          int numero1 = 0;
+         int numero2 = 0;
+         double areaFigura = 0f;
+         double ladoFigura = 0f;
+         double PI = Math.PI;
+         int tabuada = 0;
+         int moduloDivisao = 0;
+         int fatorial = 0;
+
+// Número positivo ou negativo
          System.out.println("Digite um número: ");
          numero1 = leitor.nextInt();
 
@@ -15,89 +24,75 @@ public class Main {
             System.out.println("Número negativo");
         }
 
-        int numero2 = 0;
-        int numero3 =0;
+// Comparando numeros e dizendo qual é maior
+        System.out.println("Digite um número inteiro: ");
+        numero1 = leitor.nextInt();
         System.out.println("Digite um número inteiro: ");
         numero2 = leitor.nextInt();
-        System.out.println("Digite um número inteiro: ");
-        numero3 = leitor.nextInt();
 
-        if (numero2 == numero3){
-            System.out.println("Os numeros:" + numero2 + " e " + numero3 + " são iguais");
+        if (numero1 == numero2){
+            System.out.println("Os numeros:" + numero1 + " e " + numero2 + " são iguais");
         } else {
-            System.out.println("Os numeros: " + numero2 + " e " + numero3 + " são diferentes");
+            System.out.println("Os numeros: " + numero1 + " e " + numero2 + " são diferentes");
         }
-        if (numero2 > numero3){
+        if (numero1 > numero2){
             System.out.println("O primeiro numero é maior ");
-        } else if (numero2 < numero3){
+        } else if (numero1 < numero2){
             System.out.println("O segundo numero é maior ");
         } else {
             System.out.println("Os números são iguais portanto nao há maior ou menor ");
         }
-
+// Calcular area do quadrado ou circulo
         System.out.println("###########");
         System.out.println("O digite o numero da ação desejada: ");
         System.out.println("1. Calcular área do quadrado");
         System.out.println("2. Calcular área do círculo");
         System.out.println("###########");
 
-        int numero4 = 0;
-        int areaQuadrado = 0;
-        int ladoQuadrado = 0;
-        int raioCirculo = 0;
-        float areaCirculo = 0f;
-        float PI = 3.14f;
+        numero1 = leitor.nextInt();
 
+        if (numero1 == 1){
 
-        numero4 = leitor.nextInt();
+            System.out.println("Digite o valor do lado do quadrado");
+            ladoFigura = leitor.nextDouble();
+            areaFigura = Math.pow(ladoFigura,2);
+            System.out.println("A area desse quadrado é de : " + areaFigura );
+        } else if (numero1 == 2){
 
-        if (numero4 == 1){
-
-            System.out.println("digite o valor do lado do quadrado");
-            ladoQuadrado = leitor.nextInt();
-            areaQuadrado = ladoQuadrado * ladoQuadrado;
-            System.out.println("A area desse quadrado é de : " + areaQuadrado );
-        } else if (numero4 == 2){
-
-            System.out.println("digite o valor do raio do circulo");
-            raioCirculo = leitor.nextInt();
-            areaCirculo = PI * (raioCirculo * raioCirculo);
-            System.out.println("A area desse circulo é de : " + areaCirculo);
+            System.out.println("Digite o valor do raio do circulo");
+            ladoFigura = leitor.nextDouble();
+            areaFigura = PI * Math.pow(ladoFigura,2);
+            System.out.println("A area desse circulo é de : " + areaFigura);
 
         }
 
-        int numero5 = 0;
-        int tabuada = 0;
+// Tabuada
+
         System.out.println("Digite um numero que gostaria de exibir a tabuada: " );
 
-        numero5 = leitor.nextInt();
-        for (int a = 0; a <= 10; a++ ){
-            tabuada = a * numero5;
-            System.out.println(a + " X " + numero5 + " = " + tabuada );
+        numero1 = leitor.nextInt();
+        for (int a = 1; a <= 10; a++ ){
+            tabuada = a * numero1;
+            System.out.println(a + " X " + numero1 + " = " + tabuada );
         }
 
-        int numero6 = 0;
-        int moduloDivisao =0;
+// Par ou Impar
+
         System.out.println("Digite um numero inteiro: ");
-        numero6 = leitor.nextInt();
-        moduloDivisao = numero6 % 2;
-        switch (moduloDivisao){
-            case 0:
-                System.out.println("O número é Par !");
-                break;
-            case 1:
-                System.out.println("O número é Ímpar ! ");
-                break;
-        }
+        numero1 = leitor.nextInt();
+        moduloDivisao = numero1 % 2;
+        System.out.println((moduloDivisao == 0) ? "O número é Par!" : "O número é Ímpar");
 
-        int numero7 = 0;
-        int fatorial =0;
+
+
+// Fatorial
+
         System.out.println("Fatorial Digite um numero :");
-        numero7 = leitor.nextInt();
-        fatorial = numero7;
-        while (numero7 > 1) {
-            fatorial = fatorial * (numero7 - 1);
-            numero7--;
+        numero1 = leitor.nextInt();
+        fatorial = numero1;
+        while (numero1 > 1) {
+            fatorial = fatorial * (numero1 - 1);
+            numero1--;
         }
             System.out.println("Seu fatorial é:" + fatorial);
 
